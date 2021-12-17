99d99bd5456a0f173a25d8a28a5bd35ee64dfcf5
Aya L <liu-yf18@mails.tsinghua.edu.cn>
2021-12-17 23:26:20 +0800

    0

--- MyWork/12.17/2.cpp
+++ MyWork/12.17/2.cpp
@@ -1,11 +1,25 @@
 #include<iostream>
 #include<fstream>
+#include<set>
+#include<string>
+#include<string.h>
 using namespace std;
+set<string> my_set_A,my_set_B;
 int main(){
     if(!freopen("G:\\VSProjects\\ayas_project\\MyWork\\12.17\\input_2","r",stdin)) {cout<<"no input"<<endl; return 0;}
-    int n,m;
-    while(~scanf("%d%d",&n,&m)){
-
+    int n;
+    while(~scanf("%d",&n)&&n){
+        char s1[10],s2[10];
+        memset(s1,'0',10);//memset可以重置char*
+        memset(s2,'0',10);
+        for(int i=0;i<n;i++){
+            scanf("%s %s",&s1,&s2);
+            my_set_A.insert(s1);//char* 可以被当成string处理
+            my_set_A.insert(s2);
+            my_set_B.insert(s2);
+        }
+        if(my_set_A.size()-my_set_B.size()==1) cout<<"Yes"<<endl;
+        else cout<<"No"<<endl;
     }
     return 0;
 }
---
+++ MyWork/12.17/3.cpp
@@ -0,0 +1,41 @@
+#include<fstream>
+#include<iostream>
+#include<map>
+#include<string>
+//map[key]=value;
+//时间复杂度O(logn)
+using namespace std;
+int main(){
+    if(!freopen("G:\\VSProjects\\ayas_project\\MyWork\\12.17\\input_3","r",stdin)) return 0;
+    int num_of_shop=0;
+    while(~scanf("%d",&num_of_shop)){
+        int num_of_day;
+        map<string,int> shop;
+        for(int i=0;i<num_of_shop;i++){
+            char shop_name_buff[31];
+            scanf("%s",shop_name_buff);
+            shop.insert({shop_name_buff,0});//往map里面加入元素，可以用pair<string,int>(xxx),也可以直接用中括号括起来
+        }
+        cin>>num_of_day;
+        for(int i=0;i<num_of_day;i++){
+            for(int j=0;j<num_of_shop;j++){
+                int price_value=0;
+                char shop_name_buff[31];
+                scanf("%d%s",&price_value,shop_name_buff);
+                shop[shop_name_buff]+=price_value;
+                map<string,int>::iterator it=shop.begin();
+                int rank=1;
+                for(;it!=shop.end();it++){
+                    if(it->second>shop["memory"]){
+                        rank++;
+                    }
+                }
+                cout<<rank<<endl;
+            }
+            
+        }
+        shop.clear();
+
+    }
+    return 0;
+}
